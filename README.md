33de7487f0a7648a921a967a9aca8434b11ca705
Fernando del Pino <fherdelpino@hotmail.com>
2023-03-13 00:45:41 +1100

    Add MongoDB capabilities (#2)
    * add MongoDB Repository
    
    * add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting Started
+
+Deploy to GCP with: `gcloud app deploy`
---
+++ src/main/java/com/appspot/fherdelpino/expenses/model/Expense.java
@@ -0,0 +1,15 @@
+package com.appspot.fherdelpino.expenses.model;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document("expenses")
+@Data
+public class Expense {
+    String name;
+    Double amount;
+    LocalDate date;
+    String description;
+}
