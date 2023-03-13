1f6eae9c4e18e5b25dbe00f98bf216b044a039f6
Fernando del Pino <fherdelpino@hotmail.com>
2023-03-14 10:37:07 +1100

    add get and delete by id methods

--- src/main/java/com/appspot/fherdelpino/expenses/controller/ExpenseController.java
+++ src/main/java/com/appspot/fherdelpino/expenses/controller/ExpenseController.java
@@ -1,11 +1,14 @@
 package com.appspot.fherdelpino.expenses.controller;
 
+import com.appspot.fherdelpino.error.ExpenseNotFoundException;
 import com.appspot.fherdelpino.expenses.model.Expense;
 import com.appspot.fherdelpino.expenses.repository.ExpenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +44,24 @@ public class ExpenseController {
     public List<Expense> getByDates(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
                                     @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to) {
         return expenseRepository.findByDates(from, to);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteExpense(@PathVariable String id) {
+        expenseRepository.findById(id)
+                .ifPresentOrElse(
+                        expense -> expenseRepository.deleteById(expense.getId()),
+                        () -> {
+                            throw new ExpenseNotFoundException();
+                        }
+                );
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Expense getExpense(@PathVariable String id) {
+        return expenseRepository.findById(id)
+                .orElseThrow(() -> new ExpenseNotFoundException());
     }
 
 }
