977df95745fe58f7ae02ff00ff3a1a2f8a138aee
Fernando del Pino <fherdelpino@hotmail.com>
2023-03-16 18:42:34 +1100

    fix date format

--- src/main/java/com/appspot/fherdelpino/expenses/model/Expense.java
+++ src/main/java/com/appspot/fherdelpino/expenses/model/Expense.java
@@ -1,5 +1,6 @@
 package com.appspot.fherdelpino.expenses.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -14,6 +15,7 @@ public class Expense {
     private String id;
     private String name;
     private Double amount;
+    @JsonFormat(pattern = "yyyy-M-d")
     private LocalDate date;
     private String description;
 }
