2aab6d1b41ab061c2372b5c4bbc9108bd1ac3edc
Fernando del Pino <fherdelpino@hotmail.com>
2023-06-30 03:40:56 +1000

    try gcloud with bash

--- src/main/java/com/appspot/fherdelpino/GoogleAppengineApplication.java
+++ src/main/java/com/appspot/fherdelpino/GoogleAppengineApplication.java
@@ -2,17 +2,14 @@ package com.appspot.fherdelpino;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class GoogleAppengineApplication implements CommandLineRunner {
-
-	@Value("${spring.data.mongodb.uri}")
-	private String mongoDbUri;
-
+public class GoogleAppengineApplication {
 
 	private static final Logger LOG = LoggerFactory
 			.getLogger(GoogleAppengineApplication.class);
@@ -20,9 +17,9 @@ public class GoogleAppengineApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(GoogleAppengineApplication.class, args);
 	}
-	@Override
-	public void run(String... args) {
-		LOG.info("fhdpr | mongodburi={}", mongoDbUri);
-	}
 
+	@Bean
+	ApplicationRunner applicationRunner(Environment environment) {
+		return args -> LOG.info("fhdpr | mongodburi={}", environment.getProperty("spring.data.mongodb.uri"));
+	}
 }
