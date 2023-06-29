d7166f4d1ec7a1bac4e5f0cc5299075f321315d9
Fernando del Pino <fherdelpino@hotmail.com>
2023-06-30 03:11:18 +1000

    checking variable substitution

--- src/main/java/com/appspot/fherdelpino/GoogleAppengineApplication.java
+++ src/main/java/com/appspot/fherdelpino/GoogleAppengineApplication.java
@@ -1,13 +1,28 @@
 package com.appspot.fherdelpino;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GoogleAppengineApplication {
+public class GoogleAppengineApplication implements CommandLineRunner {
+
+	@Value("${spring.data.mongodb.uri}")
+	private String mongoDbUri;
+
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(GoogleAppengineApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoogleAppengineApplication.class, args);
+	}
+	@Override
+	public void run(String... args) {
+		LOG.info("fhdpr | mongodburi={}", mongoDbUri);
 	}
 
 }
