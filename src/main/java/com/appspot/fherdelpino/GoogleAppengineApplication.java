c612ba8fb4e26bca8c93b26210ea6cbffb2f5ac5
Fernando del Pino <fherdelpino@hotmail.com>
2023-06-30 03:55:27 +1000

    revert application

--- src/main/java/com/appspot/fherdelpino/GoogleAppengineApplication.java
+++ src/main/java/com/appspot/fherdelpino/GoogleAppengineApplication.java
@@ -1,25 +1,13 @@
 package com.appspot.fherdelpino;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class GoogleAppengineApplication {
-
-	private static final Logger LOG = LoggerFactory
-			.getLogger(GoogleAppengineApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoogleAppengineApplication.class, args);
 	}
 
-	@Bean
-	ApplicationRunner applicationRunner(Environment environment) {
-		return args -> LOG.info("fhdpr | mongoDbUri={}", environment.getProperty("spring.data.mongodb.uri"));
-	}
 }
