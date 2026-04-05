73ea2a7f43d1393e355d6ca682892911df1832dd
Fernando del Pino <fherdelpino@hotmail.com>
2026-04-05 00:02:19 -0600

    Fix HelloControllerTest

--- src/test/java/com/appspot/fherdelpino/hello/controller/HelloControllerTest.java
+++ src/test/java/com/appspot/fherdelpino/hello/controller/HelloControllerTest.java
@@ -1,8 +1,12 @@
 package com.appspot.fherdelpino.hello.controller;
 
+import com.appspot.fherdelpino.security.configuration.WebSecurityConfig;
+import com.appspot.fherdelpino.security.filter.JwtAuthFilter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -11,7 +15,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Import(HelloController.class)
+@WebMvcTest(value = HelloController.class, excludeFilters = {
+        @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfig.class),
+        @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthFilter.class)
+})
 public class HelloControllerTest {
 
     @Autowired
