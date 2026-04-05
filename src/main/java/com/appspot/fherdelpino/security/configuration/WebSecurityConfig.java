d22af2daa5024df79a7b5f726c5d5d93418d5f95
Fernando del Pino <fherdelpino@hotmail.com>
2026-04-05 02:42:58 -0600

    Get /me endpoint working with cognito

--- src/main/java/com/appspot/fherdelpino/security/configuration/WebSecurityConfig.java
+++ src/main/java/com/appspot/fherdelpino/security/configuration/WebSecurityConfig.java
@@ -1,70 +1,33 @@
 package com.appspot.fherdelpino.security.configuration;
 
 
-import com.appspot.fherdelpino.security.filter.JwtAuthFilter;
-import com.appspot.fherdelpino.security.service.MongoAuthUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.appspot.fherdelpino.security.controller.AuthenticationController.AUTH_PATH;
 
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
-
-    private static final String ENCODE_ID = "bcrypt";
-
-    @Autowired
-    private MongoAuthUserDetailsService mongoAuthUserDetailsService;
-
-    @Autowired
-    private JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authorizeHttpRequests(authz -> authz
-                .requestMatchers("/hello").permitAll()
-                .requestMatchers("/ai-steen/**").permitAll()
-                .requestMatchers(AUTH_PATH).permitAll()
-                .requestMatchers(AUTH_PATH + "/**").permitAll()
-                .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
-                .requestMatchers("/expense/**").permitAll()//.hasAuthority("USER")
-                .requestMatchers("/restaurant/**").permitAll()//.hasAuthority("USER")
-                .anyRequest().authenticated()
-        );
-        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                        .requestMatchers("/hello").permitAll()
+                        .requestMatchers("/ai-steen/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
+                        .requestMatchers("/expense/**").permitAll()//.hasAuthority("USER")
+                        .requestMatchers("/restaurant/**").permitAll()//.hasAuthority("USER")
+                        .requestMatchers("/me").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(login -> login
+                        .defaultSuccessUrl("/app", true)
+                )
+                .oauth2ResourceServer(oauth -> oauth
+                        .jwt(jwt -> {
+                        })
+                );
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        builder.userDetailsService(mongoAuthUserDetailsService);
-        return builder.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        Map<String, PasswordEncoder> encoders = new HashMap<>();
-        encoders.put(ENCODE_ID, new BCryptPasswordEncoder());
-        return new DelegatingPasswordEncoder(ENCODE_ID, encoders);
     }
 }
